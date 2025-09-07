63b771685943379eafa3ae964767d429217b72a7
Prathamesh Anvekar <anvekarprathamesh13@gmail.com>
2025-09-07 20:50:02 +0000

    player appears on the screen and can now move using wasd

--- main.py
+++ main.py
@@ -3,19 +3,38 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        screen.fill("black")
+        
+        for thing in drawable:
+            thing.draw(screen)
+
+        updatable.update(dt)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
